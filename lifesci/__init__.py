372ad784ebae04e6b48b4a791cd6812923408c63
Brandon Malone <bmmalone@gmail.com>
2023-04-07 20:11:49 +0200

    UPD setup_scm for version

--- lifesci/__init__.py
+++ lifesci/__init__.py
@@ -1,2 +1,9 @@
-__version_info__ = ('0', '3', '1')
-__version__ = '.'.join(__version_info__)
+try:
+    # Try getting the version without invoking setuptools-scm
+    from ._version import version as __version__
+except (ImportError, ModuleNotFoundError):
+    try:
+        from setuptools_scm import get_version
+        __version__ = get_version()
+    except ImportError:
+        __version__ = "unknown"
